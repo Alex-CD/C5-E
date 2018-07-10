285119cb6f86d552755f47634fb9853b9d28c9e6
Alex Clarke <alexander_M_clarke@live.co.uk>
2018-07-10 11:48:05 +0100

    Add basic (broken) game api calls

--- routes/game.js
+++ routes/game.js
@@ -1,7 +1,9 @@
 
-module.exports = function(router, db, chatkit, pusher) {
+module.exports = function(router, schema, chatkit, pusher) {
+    const {check, validationResult} = require("express-validator/check");
+    const gameUtils = require.main.require('./util/game');
 
-    // Load game page (redirect to lobby)
+    // Load game page
     router.get('/game', function (req, res, next) {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.write("Load game page");
@@ -9,13 +11,67 @@ module.exports = function(router, db, chatkit, pusher) {
     });
 
 
-    // get game data
-    router.get('/game/:gameId', function (req, res, next) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.write("Join existing lobby");
-        res.end();
+    // Getting game data
+    router.get('/game/:lobbyID',
+        [
+            check('lobbyID')
+                .exists().withMessage("No Lobby name supplied.")
+                .isAlphanumeric().withMessage("Lobby name must be alphanumeric.")
+                .isLength({max: 20}).withMessage("Lobby name too long!")],
+        function (req, res, next) {
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                return res.status(422).json({errors: errors.mapped()});
+            }
+
+            gameUtils.getGame(req.params, res, schema);
+
+        });
+
+
+    // Remove player (by removing sessionID, slot is rejoinable)
+    router.post('/game/:lobbyID/leaveSlot', [
+        check('lobbyID')
+            .isAlphanumeric().withMessage("Lobby name must be alphanumeric.")
+            .isLength({max: 20}).withMessage("Lobby name too long!")
+    ], function (req, res){
+
+        const errors = validationResult(req);
+
+
+        if (!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.mapped()});
+        }
+
+        gameUtils.leaveSlot(req.params.lobbyID, req.sessionID, res, schema, pusher);
     });
 
 
 
+    // Join into slot while game is underway
+    router.post("/lobby/:lobbyID/joinSlot", [
+            check('civName')
+                .exists().withMessage("No civ name supplied")
+                .isAlphanumeric().withMessage("Civ name invalid")
+                .isLength({max: 20}).withMessage("Civ name too long"),
+            check('playerID')
+                .exists().withMessage("No name supplied!")
+                .isAlphanumeric().withMessage("No username supplied")
+                .isLength({max: 20}).withMessage("Username too long."),
+            check('lobbyID')
+                .isAlphanumeric().withMessage("lobbyID not alphanumeric")
+                .isLength({max: 20}).withMessage("lobbyID too long")],
+        function (req, res, next) {
+
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                return res.status(422).json({errors: errors.mapped()});
+            }
+
+
+            gameUtils.joinSlot(req.params.lobbyID, req.query.playerID, req.query.civName, req.sessionID, res, schema, pusher);
+
+        });
 };
--- util/game.js
+++ util/game.js
@@ -3,6 +3,71 @@
 // Exported Methods //
 //////////////////////
 
+module.exports.getGame = function getGame(params, res, schema){
+
+    schema.lobby.find({ lobbyID: params.lobbyID }, (err, lobbies) => {
+        if(lobbies.length > 0){
+            res.send(lobbies[0]);
+        }
+        else {
+            return res.status(409).json({errors: "Game does not exist."})
+        }
+    });
+};
+
+
+module.exports.joinSlot = function joinSlot(lobbyID, playerID, civName, sessionID, res, schema, pusher){
+    schema.player.create({playerID: playerID, sessionID: sessionID, civName: civName }, (err, toAdd )=>{
+        if (err) {
+            console.log(err);
+            res.write(err);
+        }
+
+
+        schema.lobby.findOneAndUpdate({ lobbyID: lobbyID},
+            {$push: { players: toAdd }},
+            (err)=> {
+                if (err) {
+                    console.log(err);
+                    res.write(err);
+                }
+
+                // Notifying clients of lobby change
+                pusher.trigger(lobbyID+"-lobby", 'state-update', {});
+
+                res.status(201).json(lobbyID);
+            }
+        );
+    });
+};
+
+
+module.exports.leaveSlot = function leaveSlot(lobbyID, sessionID, res, schema, pusher){
+    schema.lobby.findOneAndUpdate(
+        {
+            lobbyID: lobbyID,
+            players: {$elemMatch: { sessionID: sessionID}}
+        },
+
+        {$set: {"players.$.playerID": "", "players.$.sessionID": ""}},
+
+        (err) => {
+            if(err){
+                console.log(err);
+                res.status(404).json({ errors: err.mapped()});
+            }
+
+            // Notifying clients of lobby change
+            pusher.trigger(lobbyID+"-lobby", 'state-update', {});
+
+            res.status(201).json(lobbyID)
+        });
+};
+
+
+
+
+
 
 
 //////////////////////
